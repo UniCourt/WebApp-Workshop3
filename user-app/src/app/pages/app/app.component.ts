cae13a4e26a0cbc3b2ef869444baddfba4c07c7d
Navya661 <20cs093.navyashree@sjec.ac.in>
2023-05-26 16:40:44 +0530

    Workshop 3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name : string ="Navyashree";  
+   names = ['Arun', 'Mark', 'Smith', 'Jack'];
+   show :boolean = true;
+   isError : boolean = true;
+   userName :string ="";
+
+
+   constructor() {
+    console.log('Constructor called');
+  }  
+  changeName(){
+    this.name="Steve";
+  }
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+  ngOnInit() {
+    console.log('on init called');
+  }
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+
+
+
+}
+
