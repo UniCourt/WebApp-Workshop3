d6720a5f4d38bbba40896fc883eb1417cc9c58d4
JO-HNSON <johnsondsouza255@gmail.com>
2023-05-26 16:44:41 +0530

    meassage

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name : String = "Johnson";
+  show : boolean =true;
+  names = ['Arun', 'Mark', 'Smith', 'Jack'];
+  isError : boolean = true;
+  username : String ="";
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  changeName()
+{
+  this.name="steve";
+  console.log(this.username);
+}
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+
+  ngOnInit() {
+    console.log('on init called');
+  }
+
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+}
+
--- user-app/src/app/pages/user-list/user-list.component.ts
+++ user-app/src/app/pages/user-list/user-list.component.ts
@@ -7,4 +7,32 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {}
+export class UserListComponent {
+  /*users :User[] = [
+    {
+      name : 'Johnson',
+      city : 'Mangalore',
+      emailId: 'johnson@gmail.com',
+      id:1,
+    },
+    {
+      name : 'shaaa',
+      city : 'kutthi',
+      emailId: 'shaaa@gmail.com',
+      id:1,
+    },
+    {
+      name : 'Jithin',
+      city : 'baindoor',
+      emailId: 'jithin@gmail.com',
+      id:1,
+    },
+  ]*/                    
+  constructor (public userService:UserService){}
+  ngOnInit()
+  {
+    this.userService.getUsers();
+  }
+}
+
+
