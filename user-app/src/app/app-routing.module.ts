058e7f912816639ab5f7624825dac900df129f59
Anish-Shobith <anish.shobith19@gmail.com>
2023-05-26 16:40:45 +0530

    feat: workshop-3

--- user-app/src/app/app-routing.module.ts
+++ user-app/src/app/app-routing.module.ts
@@ -7,10 +7,32 @@ import { UserDetailComponent } from './pages/user-detail/user-detail.component';
 import { CreateUserComponent } from './pages/create-user/create-user.component';
 import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'create-user',
+    component: CreateUserComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'user-detail',
+    component: UserDetailComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+
+
+export class AppComponent {
+  name: string = 'Anish Shobith P S';
+  show: boolean = true;
+  names: Array<string> = ['Arun', 'Mark', 'Smith', 'Jack'];
+  isError: boolean = false;
+  username: string = "";
+
+  constructor() {
+    console.log('Constructor called');
+  }
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+  ngOnInit() {
+    console.log('on init called');
+  }
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+
+  changeName() {
+    this.name = this.username.length ? this.username : 'Anish Shobith P S';
+  }
+}
