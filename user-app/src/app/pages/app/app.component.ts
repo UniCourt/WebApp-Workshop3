f1116427808e94284c8af1a77b2b0585eb1be112
Ralweena <loboralweenarishal@gmail.com>
2023-05-26 16:41:40 +0530

    Workshop 3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+
+
+export class AppComponent {
+  name: string = 'Ralweena Lobo';
+
+  show: boolean = true;
+  names = ['Arun', 'Mark', 'Smith', 'Jack'];
+  isError: boolean = false;
+  username : string = "";
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  changeName() {
+    this.name=this.username;
+  }
+}
