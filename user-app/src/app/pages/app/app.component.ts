b556dccf84e190064aa8e3b15502a8987eee9b64
Sanmitha <sanmishetty21@gmail.com>
2023-05-26 16:42:34 +0530

    Workshop3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name : string="Sanmitha";  
+
+  show : boolean = false;
+
+  names = ['Arun', 'Mark', 'Smith', 'Jack'];
+  isError : boolean = true;
+  userName : string = "";
+
+
+  constructor() {
+    console.log('Constructor called');
+  }
+  changeName(){
+    this.name=this.userName;
+    console.log(this.userName);
+  }
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+  ngOnInit() {
+    console.log('on init called');
+  }
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+
+}
