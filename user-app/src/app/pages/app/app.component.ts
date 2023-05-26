81d13061f7e52d1974ccd5474369ec0411f3d484
Jenihacker <20cs064.jenison@sjec.ac.in>
2023-05-26 16:39:42 +0530

    unicourt session 3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name : string = 'Jenison Monteiro';
+  show : boolean = true;
+  names = ['Arun', 'Mark', 'Smith', 'Jack'];
+  userName : string = "";
+  isError : boolean = true;
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  changeName() {
+    this.name = 'Jenison';
+    console.log(this.userName);
+  }
+
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+
+  ngOnInit() {
+    console.log('on init called');
+  }
+
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+}
--- user-app/src/app/services/user.service.ts
+++ user-app/src/app/services/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User, UserDetail } from '../model/common.dto';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root',
@@ -7,19 +9,74 @@ import { User, UserDetail } from '../model/common.dto';
 export class UserService {
   baseUrl: string = 'https://jsonplaceholder.typicode.com/users';
   userId: number;
-  users: User[] = [];
+  users = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   userAleadyAdded(): boolean {
-    return true;
+    this.users = JSON.parse(localStorage.getItem('users')) || [];
+    return this.users.length == 0 ? false : true;
   }
 
-  getUsers(): void {}
+  getUsers(): void {
+    this.users = [];
+   this.http.get(this.baseUrl).subscribe({
+     next: (response: any) => {
+       let tempUser = [];
+       response.forEach((element) => {
+         let user: User = {
+           id: element.id,
+           name: element.name,
+           city: element.address.city,
+           emailId: element.email,
+         };
+         tempUser.push(user)
+       });
 
-  getUserDetail(id: number) {}
+       localStorage.setItem("users",JSON.stringify(tempUser));
+       this.users =  JSON.parse(localStorage.getItem("users")) || [];
+     },
+     error: (err) => {
+       console.log('error', err);
+     },
+   });
+  }
 
-  addUser(user: UserDetail): void {}
+  getUserDetail(id: number) {
+    return this.http.get(this.baseUrl + '/' + id);
+  }
 
-  deleteUser(id: number): void {}
+  addUser(user: UserDetail): void {
+    this.http
+     .post(this.baseUrl, {
+       user: user,
+     })
+     .subscribe({
+       next: (response) => {
+         this.users = JSON.parse(localStorage.getItem('users')) || [];
+         this.users.push(user);
+         localStorage.setItem('users', JSON.stringify(this.users));
+         alert('User added successfully');
+         //this.router.navigateByUrl('/');
+       },
+       error: (error) => {
+         console.log('add user error', error);
+       },
+     });
+  }
+
+  deleteUser(id: number): void {
+    this.http.delete(this.baseUrl + '/' + id).subscribe({
+      next: (response) => {
+        this.users = JSON.parse(localStorage.getItem('users')) || [];
+        this.users = this.users.filter((user) => user['id'] != id);
+        localStorage.setItem('users', JSON.stringify(this.users));
+        this.users = JSON.parse(localStorage.getItem('users')) || [];
+        alert('User delete successfully');
+      },
+      error: (error) => {
+        console.log('delete user error', error);
+      },
+    });
+  }
 }
