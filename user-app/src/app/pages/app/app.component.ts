cd04ee9f38697e460cfb2c5493aa7c87f03ad392
Deonne <20cs040.deonne@sjec.ac.in>
2023-05-26 16:39:21 +0530

    Workshop 3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name:string ="Deonne";
+  show: boolean =false;
+  names = ['Arun', 'Mark', 'Smith', 'Jack'];
+  isError: boolean=true;
+  userName:string="";
+
+constructor() {
+  console.log('Constructor called');
+}
+changeName(){
+  this.name="Steve";
+  console.log(this.userName)
+}
+ngOnChanges() {
+  console.log('on changes called');
+}
+ngOnInit() {
+  console.log('on init called');
+}
+ngOnDestroy() {
+  console.log('on destroy called');
+}
+}
