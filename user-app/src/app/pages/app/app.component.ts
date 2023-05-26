4956768b074e03398d6070c5554e03365b2231fa
nishadlima <20cs098.nisha@sjec.ac.in>
2023-05-26 16:41:46 +0530

    Workkshop3

--- user-app/src/app/pages/app/app.component.ts
+++ user-app/src/app/pages/app/app.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  name: string="NISHA";
+  isError :boolean=true;
+  show: boolean=true;
+  userName :string="";
+  names = ['Nisha', 'Mark', 'hello', 'Jack'];
+  constructor() {
+    console.log('Constructor called');
+  }
+  changeName(){
+    this.name="raksha";
+    console.log(this.userName);
+  }
+  ngOnChanges() {
+    console.log('on changes called');
+  }
+  ngOnInit() {
+    console.log('on init called');
+  }
+  ngOnDestroy() {
+    console.log('on destroy called');
+  }
+
+}
